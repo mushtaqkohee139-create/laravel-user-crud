079553488ba716ba0a74dd3fff0e1e750c35b54b
mushtaqkohee139-create <mushtaqkohee139@gmail.com>
2026-01-14 16:18:30 +0500

    Handle Alert messages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -66,6 +66,13 @@
                 @endif
             </tbody>
         </table>
+        @if (session('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+
     </div>
 </div>
 
